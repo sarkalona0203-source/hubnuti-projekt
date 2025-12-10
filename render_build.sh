ae4d242528df7743f228aea2327c64aaf3801096
sarkalona0203-source <sarkisana1993@gmail.com>
2025-12-10 17:36:11 +0100

    Fix unused API_URL import in JidloCard

--- render_build.sh
+++ render_build.sh
@@ -1,2 +1,12 @@
 #!/bin/bash
 set -e
+
+# --- 1) Фронтенд ---
+cd frontend
+npm install
+npm run build
+
+# --- 2) Бэкенд ---
+cd ../backend
+pip install -r requirements.txt
+python manage.py collectstatic --noinput
