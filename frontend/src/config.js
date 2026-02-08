5c4493961678208328389c0570b1ff3883550ca0
sarkalona0203-source <sarkisana1993@gmail.com>
2026-02-08 08:10:46 +0100

    Fix start command / serve script

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,16 +1,7 @@
-/// API_URL
-export const API_URL =
-  process.env.REACT_APP_API_URL || "http://localhost:8000/api";
-
-// MEDIA_URL — ТОЛЬКО для реальных картинок из Django
-export const MEDIA_URL =
-  process.env.REACT_APP_MEDIA_URL ||
-  API_URL.replace(/\/api$/, "") + "/media";
-
 export const getImageUrl = (obrazek_url) => {
   if (!obrazek_url) return null;
   if (obrazek_url.startsWith("http")) return obrazek_url;
 
-  // public/media
+  // КАРТИНКИ ВСЕГДА С ФРОНТА
   return `/media/${obrazek_url.replace(/^\/+/, "")}`;
 };
