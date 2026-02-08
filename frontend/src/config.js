704eacbe06bb9a2b47abe57e0366ad0cb678f6d1
sarkalona0203-source <sarkisana1993@gmail.com>
2026-02-08 14:42:30 +0100

    Fix start command / serve script

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,13 +1,15 @@
+// config.js
+
 export const API_URL =
   process.env.REACT_APP_API_URL ||
-  "https://hubnuti-projekt-15.onrender.com/api";
+  "https://hubnuti-projekt-16.onrender.com/api";
 
-export const MEDIA_BASE_URL =
-  process.env.REACT_APP_MEDIA_URL ||
+// 👇 ЯВНО указываем фронт с картинками
+export const FRONTEND_MEDIA_URL =
   "https://hubnuti-projekt-15.onrender.com/media";
 
 export const getImageUrl = (obrazek_url) => {
   if (!obrazek_url) return null;
   if (obrazek_url.startsWith("http")) return obrazek_url;
-  return `${MEDIA_BASE_URL}/${obrazek_url.replace(/^\/+/, "")}`;
+  return `${FRONTEND_MEDIA_URL}/${obrazek_url.replace(/^\/+/, "")}`;
 };
