14fee63f099ff7a6105def4a2a9f996c067d7a92
sarkalona0203-source <sarkisana1993@gmail.com>
2026-02-08 14:54:03 +0100

    Fix start command / serve script

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,12 +1,17 @@
 // config.js
 
+export const API_URL =
+  process.env.REACT_APP_API_URL ||
+  "https://hubnuti-projekt-15.onrender.com/api";
+
+// ⬇️ ЕДИНСТВЕННЫЙ источник картинок
 export const FRONTEND_MEDIA_URL =
   "https://hubnuti-projekt-15.onrender.com/media";
 
 export const getImageUrl = (obrazek_url) => {
   if (!obrazek_url) return null;
 
-  // 🔥 если пришёл полный URL — вырезаем всё до /media/
+  // если пришёл полный URL — вырезаем всё до /media/
   const filename = obrazek_url.includes("/media/")
     ? obrazek_url.split("/media/").pop()
     : obrazek_url;
