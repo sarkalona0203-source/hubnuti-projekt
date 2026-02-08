0b4010a46db1ce0de12d7eaac9c1ff39ea8fed37
sarkalona0203-source <sarkisana1993@gmail.com>
2026-02-08 08:12:40 +0100

    Fix start command / serve script

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,7 +1,12 @@
+// config.js
+
+export const API_URL =
+  process.env.REACT_APP_API_URL || "http://localhost:8000/api";
+
 export const getImageUrl = (obrazek_url) => {
   if (!obrazek_url) return null;
   if (obrazek_url.startsWith("http")) return obrazek_url;
 
-  // КАРТИНКИ ВСЕГДА С ФРОНТА
+  // картинки ТОЛЬКО из frontend/public/media
   return `/media/${obrazek_url.replace(/^\/+/, "")}`;
 };
