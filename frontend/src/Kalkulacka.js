41476437835c56547a4f8254671ddd97f420a209
sarkalona0203-source <sarkisana1993@gmail.com>
2026-02-08 08:35:20 +0100

    Fix start command / serve script

--- frontend/src/Kalkulacka.js
+++ frontend/src/Kalkulacka.js
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import "./kalkulacka.css";
-import { API_URL, getImageUrl } from "./config";
+import { API_URL } from "./config";
 
 const INITIAL_FORM = {
   vaha: "",
